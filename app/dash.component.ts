3b602df7bb653943f6f5ca5998122677bc4dac71
Dan Kostiuk <dank216@gmail.com>
2016-11-26 12:21:38 -0500

    Added Google Charts in dashboard with sample lineChart data

--- app/dash.component.ts
+++ app/dash.component.ts
@@ -3,14 +3,34 @@ import { Subscription } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SummaryService }       from './summary.service';
 
+import { GoogleChart } from '../directives/angular2-google-chart.directive';
+
 @Component({
   selector: 'dash',
+  directives: [GoogleChart],
   templateUrl: 'app/dash.component.html',
   styleUrls: ['app/dash.component.css']
 
 })
 
 export class DashComponent implements OnInit {
+
+    public line_ChartData = [
+        ['Date', 'CO2e'],
+        ['2016-11-20',  265.15],
+        ['2016-11-21',  259.11],
+        ['2016-11-22',  248.59],
+        ['2016-11-23',  256.1],
+        ['2016-11-24',  242.36],
+        ['2016-11-25',  240.5],
+        ['2016-11-26',  236.12]];
+
+    public line_ChartOptions = {
+        title: 'Last 7 Days CO2e',
+        curveType: 'function',
+        legend: { position: 'bottom'
+        }
+    };
 
   private subscription: Subscription;
   private code: string;
@@ -29,12 +49,18 @@ export class DashComponent implements OnInit {
         (param: any) => {
           this.code = param['code'];
 
+          localStorage.setItem('id_token', this.code);
+
           this.summaryService.getSummary(this.code)
               .then(response => {
                   this.name = 'Hello ' + response.name + ',';
                   this.summary = response.message;
                   this.summaryType = response.summaryType;
                   this.userId = response.userId;
+
+
+                  //TODO: add chart here with fake data
+
               });
         });
   }
